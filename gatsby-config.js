f1bd5dbe590aced7ae8e5cfd187303a8fad72079
Raymond Larkin <jots.larkin.rl@gmail.com>
2022-09-24 07:56:17 -0400

    Got through part 3 which won't use images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
   siteMetadata: {
+	  title: 'Maybe Useful Gatsby Time Investment',
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
     return (
         <Layout pageTitle="Home Page">
             <p>I'm making this by following the Gatsby Tutorial.</p>
+            <StaticImage 
+                alt="Something about Git"
+                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.FNpXhheOh4iw8ARx2-IbKAHaFs%26pid%3DApi&f=1&ipt=b96793e6e50de5520ba32890a63068554035e1776bb6a148151c9a7c7fa660d4&ipo=images"
+            />
         </Layout>
     )
 }
